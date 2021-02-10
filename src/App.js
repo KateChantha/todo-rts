4b634da100730289821a2f265192810c30708dfa
Kato <kubkate@gmail.com>
2021-02-09 17:45:46 -0800

    implement edit task name

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ const App = (props) => {
       name: name, 
       completed: false 
     };
-    console.log("id", newTask.id)
+    
     setTasks([...tasks, newTask]);
   }
 
   const toggleTaskCompleted = (id)=> {
-    console.log("toggle id", id)
     // change the completed property of only the task that was toggled
     const updatedTasks = tasks.map(task => {
       return id === task.id 
@@ -30,45 +29,55 @@ const App = (props) => {
   }
 
   const deleteTask = (id) => {
-    console.log("delete id", id)
-    const remainingTask = tasks.filter(task => id !== task.id)
+    const remainingTasks = tasks.filter(task => id !== task.id)
 
-    setTasks(remainingTask);
+    setTasks(remainingTasks);
+  }
+
+  const editTask = (id, newName) => {
+    const editedTasks = tasks.map(task => {
+      return id === task.id
+            ? {...task, name: newName}
+            : task;
+    })
+    setTasks(editedTasks);
   }
 
   /** {...task} short handed for <Todo id={task.id} name={task.name} completed={task.completed} /> */
   const taskList = tasks.map(task => (
     <Todo 
+      {...task} 
       key={task.id} 
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
-      {...task} 
+      editTask={editTask}
     />
   ))
   
   return (
     <React.StrictMode>
       <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form addTask={addTask}/>
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      
-      <h2 id="list-heading">
-      { `${taskList.length} tasks remaining` }
-      </h2>
+        <h1>TodoMatic</h1>
+        <Form addTask={addTask}/>
 
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        {taskList}
-      </ul>
-    </div>
+        <div className="filters btn-group stack-exception">
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
+        </div>
+        
+        <h2 id="list-heading">
+          { `${taskList.length} tasks remaining` }
+        </h2>
+
+        <ul
+          role="list"
+          className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading"
+        >
+          {taskList}
+        </ul>
+      </div>
     </React.StrictMode>
   );
 };
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,32 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
 export default function Todo(props) {
-  const {id, completed, toggleTaskCompleted, deleteTask} = props;
-  return (
-    <li className="todo stack-small">
-      <div className="c-cb">
+  const {id, name, completed, toggleTaskCompleted, deleteTask, editTask} = props;
+
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={id}>
+          New name for {name}
+        </label>
         <input 
           id={id} 
-          type="checkbox" 
-          defaultChecked={completed}
-          onChange={() => toggleTaskCompleted(id)}
+          className="todo-text" 
+          type="text" 
+          onChange={e => setNewName(e.target.value)}
         />
-        <label className="todo-label" htmlFor="todo-2">
-          {props.name}
-        </label>
       </div>
 
       <div className="btn-group">
-        <button type="button" className="btn">
-          Edit <span className="visually-hidden">Repeat</span>
-        </button>
         <button 
           type="button" 
-          className="btn btn__danger"
-          onClick={() => deleteTask(id)}
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
         >
-          Delete <span className="visually-hidden">Repeat</span>
+          Cancel
+          <span className="visually-hidden">renaming {name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {name}</span>
         </button>
       </div>
+    </form>
+  );
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+          <input
+            id={id}
+            type="checkbox"
+            defaultChecked={completed}
+            onChange={() => toggleTaskCompleted(id)}
+          />
+          <label className="todo-label" htmlFor={id}>
+            {name}
+          </label>
+        </div>
+        <div className="btn-group">
+          <button 
+            type="button" 
+            className="btn"
+            onClick={() => setEditing(true)}
+          >
+            Edit <span className="visually-hidden">{name}</span>
+          </button>
+          <button
+            type="button"
+            className="btn btn__danger"
+            onClick={() => deleteTask(id)}
+          >
+            Delete <span className="visually-hidden">{name}</span>
+          </button>
+        </div>
+    </div>
+  );
+
+  return (
+    <li className="todo">
+      {isEditing ? editingTemplate : viewTemplate}
     </li>
   )
 }
