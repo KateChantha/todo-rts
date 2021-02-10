80eca49ab59d32c9ad540f1ba55e7bdded097645
Kato <kubkate@gmail.com>
2021-02-09 16:07:15 -0800

    implement add todo to taskList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-const App = ({tasks}) => {
+const App = (props) => {
+  const [tasks, setTasks] = useState(props.tasks)
+
+  const addTask = (name) => {
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
 
   /**short handed for <Todo id={task.id} name={task.name} completed={task.completed} /> */
   const taskList = tasks.map(task => <Todo key={task.id} {...task} />)
@@ -10,43 +18,13 @@ const App = ({tasks}) => {
     <React.StrictMode>
       <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-
+      
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
@@ -56,9 +34,6 @@ const App = ({tasks}) => {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        {/* <Todo name='eat' completed={true} id="todo-0"/>
-        <Todo name='sleep' completed={false} id="todo-1"/>
-        <Todo name='repeat' completed={false} id="todo-2"/> */}
         {taskList}
       </ul>
     </div>
